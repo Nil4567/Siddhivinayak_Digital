27f44afc5bda788fb6c0c0bec4958b6db8201aa1
Nil4567 <venturessuper@gmail.com>
2025-12-09 00:17:27 +0530

    Add files via upload

---
+++ pages/config.js
@@ -0,0 +1,3 @@
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzstPJX9Oh-KN_vRQJOFssuAjhFTanjaVjHb-Rp_mYdiLShmM_56J2SNAR_vlNz7RV/exec";
+
+export const SECURITY_TOKEN = "SIDDHIVINAYAK123";
