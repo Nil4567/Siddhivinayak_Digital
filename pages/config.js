bfed8c93d39ade4753fd1f7b549e5b4c8c387272
Nil4567 <venturessuper@gmail.com>
2025-12-09 00:59:55 +0530

    Update config.js

--- pages/config.js
+++ pages/config.js
@@ -18,6 +18,9 @@ export const SECURITY_TOKEN = "SIDDHIVINAYAK123";
 /**
  * 🔥 Default redirect paths
  */
-export const APP_BASE = "/Siddhivinayak_Digital/";
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzvsxBRxAHMshglml3oFArYKm4Ag1-REw_R15t6WctODXk-etN_CLCp7b4qV2-4NmV7/exec";
+
+export const SECURITY_TOKEN = "SIDDHIVINAYAK123";
+
 export const DASHBOARD_PAGE = "/Siddhivinayak_Digital/pages/dashboard.html";
-export const LOGIN_PAGE = "/Siddhivinayak_Digital/pages/login.html";
+
