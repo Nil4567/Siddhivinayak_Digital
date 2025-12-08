5c76f5f7bd5063c449d5ff2cedbab9172f4c4912
Nil4567 <venturessuper@gmail.com>
2025-12-09 00:17:08 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzstPJX9Oh-KN_vRQJOFssuAjhFTanjaVjHb-Rp_mYdiLShmM_56J2SNAR_vlNz7RV/exec";
-
-export const SECURITY_TOKEN = "SIDDHIVINAYAK123";
