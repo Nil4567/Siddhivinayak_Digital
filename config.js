ae49225881cb76d0dd415be3995c677d8a7112bb
Nil4567 <venturessuper@gmail.com>
2025-12-09 00:01:31 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ================================
+// GLOBAL CONFIGURATION FILE
+// ================================
+
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzVcME3Xb95pDU8faZ1HhGGB1k5hYiBhSlx6GPFUcE2CbCtzO5_9Y3KLv12aoFF70M8sQ/exec";
+const SECURITY_TOKEN = "SUPERADMIN-TOKEN-123456";  // Change to your actual token
