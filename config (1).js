e73918c669bab150c120de5c434df9c9f8ce7712
Nil4567 <venturessuper@gmail.com>
2025-12-09 20:22:45 +0530

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,19 @@
+/*******************************************************
+ * GLOBAL CONFIG — USED BY ALL FRONTEND PAGES
+ *******************************************************/
+
+/**
+ * Your Google Apps Script URL
+ */
+export const SCRIPT_URL =
+    "https://script.google.com/macros/s/AKfycbzvsxBRxAHMshglml3oFArYKm4Ag1-REw_R15t6WctODXk-etN_CLCp7b4qV2-4NmV7/exec";
+
+/**
+ * Security Token
+ */
+export const SECURITY_TOKEN = "SIDDHIVINAYAK123";
+
+/**
+ * Redirect after login (GitHub Pages FIX)
+ */
+export const DASHBOARD_PAGE = "/pages/dashboard.html";
