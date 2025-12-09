1c9c5549e7eff5797218ccdeaecf293347b522a1
Nil4567 <venturessuper@gmail.com>
2025-12-09 22:56:57 +0530

    Update user-management.js

--- pages/user-management.js
+++ pages/user-management.js
@@ -3,7 +3,9 @@ const SUPABASE_URL = "https://qcyqjcxzytjtsikzrdyv.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeXFqY3h6eXRqdHNpa3pyZHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzA4NjAsImV4cCI6MjA4MDgwNjg2MH0.q0gkhSgqT_BNfsZBCd2stkgskf2V-CDVIG9p6S5LHdM";
 
+// 🚀 CORRECT Supabase client (CDN)
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 // HARD-CODED Page Redirects
 const LOGIN_PAGE = "/Siddhivinayak_Digital/pages/login.html";
