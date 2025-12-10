d146a6e2789bcc5bf336ec6083c3dd9596420e4e
Nil4567 <venturessuper@gmail.com>
2025-12-10 20:38:14 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// supabase-config.js (place at repo root)
-export const SUPABASE_URL = "https://qcyqjcxzytjtsikzrdyv.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeXFqY3h6eXRqdHNpa3pyZHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzA4NjAsImV4cCI6MjA4MDgwNjg2MH0.q0gkhSgqT_BNfsZBCd2stkgskf2V-CDVIG9p6S5LHdM";
-export const DASHBOARD_PAGE = "/Siddhivinayak_Digital/pages/dashboard.html";
