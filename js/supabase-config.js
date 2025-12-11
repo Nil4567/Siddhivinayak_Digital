6abdaab3dd124d1c642bad0d8a5e1f96e1f1c4f7
Nil4567 <venturessuper@gmail.com>
2025-12-11 22:16:40 +0530

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,6 @@
+// js/supabase-config.js
+const supabaseUrl = "https://qcyqjcxzytjtsikzrdyv.supabase.co";  
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeXFqY3h6eXRqdHNpa3pyZHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzA4NjAsImV4cCI6MjA4MDgwNjg2MH0.q0gkhSgqT_BNfsZBCd2stkgskf2V-CDVIG9p6S5LHdM";  
+
+const { createClient } = supabase;
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
