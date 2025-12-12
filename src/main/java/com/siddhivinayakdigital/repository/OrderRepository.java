d91069e2903a025820827fd0e722e2979e9b961e
Nil4567 <venturessuper@gmail.com>
2025-12-12 16:06:32 +0530

    Delete src/main/java/com/siddhivinayakdigital directory

--- src/main/java/com/siddhivinayakdigital/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.siddhivinayakdigital.repository;
-
-import com.siddhivinayakdigital.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    Order findByOrderId(String orderId);
-}
