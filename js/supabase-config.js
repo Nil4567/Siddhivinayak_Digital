8599f15a4c9a8c249eb6cd4ce340a56d1356c274
Nil4567 <venturessuper@gmail.com>
2025-12-12 17:26:08 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,5 @@
 // js/supabase-config.js
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 const supabaseUrl = "https://qcyqjcxzytjtsikzrdyv.supabase.co";  
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeXFqY3h6eXRqdHNpa3pyZHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMzA4NjAsImV4cCI6MjA4MDgwNjg2MH0.q0gkhSgqT_BNfsZBCd2stkgskf2V-CDVIG9p6S5LHdM";  
 
