da0661d5da68ec37459f7d852502b8967679408a
Nil4567 <venturessuper@gmail.com>
2025-12-14 23:28:17 +0530

    Delete src/main/java/com/siddhivinayakdigital/repository/OrderHistoryRepository.java

--- src/main/java/com/siddhivinayakdigital/repository/OrderHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.siddhivinayakdigital.repository;
-
-import com.siddhivinayakdigital.entity.OrderHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
-}
